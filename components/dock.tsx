fd55c8f052a5f79c4164a482d6794234ce8c2243
nethMesabothh <nethmesaboth20@gmail.com>
2025-11-27 15:31:34 +0700

    feat: create zustand and state for open folder

--- components/dock.tsx
+++ components/dock.tsx
@@ -1,33 +1,112 @@
 "use client";
 
 import { dockApps } from "@/constants";
-import { useRef } from "react";
+import { RefObject, useRef } from "react";
+import Image from "next/image";
+import { Tooltip } from "react-tooltip";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import useWindowStore from "@/store/window";
 
 const Dock = () => {
-  const dockRef = useRef(null);
+	const { openWindow, closeWindow, windows } = useWindowStore();
+	const dockRef: RefObject<HTMLDivElement | null> = useRef(null);
 
-  const toggleApp = ({ id, canOpen }: { id: string; canOpen: boolean }) => {};
-  return (
-    <section id="dock">
-      <div ref={dockRef} className="dock-container">
-        {dockApps.map(({ id, name, icon, canOpen }) => {
-          return (
-            <div key={id} className="relative flex justify-center">
-              <button
-                type="button"
-                className="dock-icon"
-                aria-label={name}
-                data-tooltip-id="dock-tooltip"
-                data-tooltip-content={name}
-                data-tooltip-delay-show={150}
-                disabled={!canOpen}
-                onClick={() => toggleApp({ id, canOpen })}
-              ></button>
-            </div>
-          );
-        })}
-      </div>
-    </section>
-  );
+	useGSAP(() => {
+		const dock: HTMLDivElement | null = dockRef.current;
+
+		if (!dock) return () => {};
+
+		const icons = dock.querySelectorAll(".dock-icon");
+
+		const animateIcons = (mouseX: number) => {
+			const { left } = dock.getBoundingClientRect();
+
+			icons.forEach((icon) => {
+				const { left: iconLeft, width } = icon.getBoundingClientRect();
+				const center = iconLeft - left + width / 2;
+				const distance = Math.abs(mouseX - center);
+
+				const intensity = Math.exp(-(distance ** 2.5) / 20000);
+
+				gsap.to(icon, {
+					scale: 1 + 0.25 * intensity,
+					y: -15 * intensity,
+					duration: 0.2,
+					ease: "power1.out",
+				});
+			});
+		};
+
+		const handleMouseMove = (e: MouseEvent) => {
+			const { left } = dock.getBoundingClientRect();
+
+			animateIcons(e.clientX - left);
+		};
+
+		const resetIcons = () => {
+			icons.forEach((icon) =>
+				gsap.to(icon, {
+					scale: 1,
+					y: 0,
+					duration: 0.3,
+					ease: "power1.out",
+				})
+			);
+		};
+
+		dock.addEventListener("mousemove", handleMouseMove);
+		dock.addEventListener("mouseleave", resetIcons);
+
+		return () => {
+			dock.removeEventListener("mousemove", handleMouseMove);
+			dock.removeEventListener("mouseleave", resetIcons);
+		};
+	}, []);
+
+	const toggleApp = ({ id, canOpen }: { id: string; canOpen: boolean }) => {
+		if (!canOpen) return;
+
+		const window = windows[id];
+
+		if (window.isOpen) {
+			closeWindow(id);
+		} else {
+			openWindow(id);
+		}
+
+		console.log(windows);
+	};
+	return (
+		<section id="dock">
+			<div ref={dockRef} className="dock-container">
+				{dockApps.map(({ id, name, icon, canOpen }) => {
+					return (
+						<div key={id} className="relative flex justify-center">
+							<button
+								type="button"
+								className="dock-icon"
+								aria-label={name}
+								data-tooltip-id="dock-tooltip"
+								data-tooltip-content={name}
+								data-tooltip-delay-show={150}
+								disabled={!canOpen}
+								onClick={() => toggleApp({ id, canOpen })}
+							>
+								<Image
+									src={`/images/${icon}`}
+									alt={name}
+									fill
+									loading={"lazy"}
+									className={canOpen ? "" : "opacity-60"}
+								/>
+							</button>
+						</div>
+					);
+				})}
+				<Tooltip id="dock-tooltip" place={"top"} className={"tooltip"} />
+			</div>
+		</section>
+	);
 };
 export default Dock;
--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,38 @@
 import { FONT_WEIGHTS } from "@/components/welcom";
+import { WINDOW_CONFIG } from "@/constants";
 
 export type FontWeightType = keyof typeof FONT_WEIGHTS;
 
 export type RenderTextType = {
-  text: string;
-  className: string;
-  baseWeight?: number;
+	text: string;
+	className: string;
+	baseWeight?: number;
 };
 
 export type AnimateLetterType = {
-  letter: HTMLSpanElement;
-  weight: number;
-  duration?: number;
+	letter: HTMLSpanElement;
+	weight: number;
+	duration?: number;
 };
 
 export type SetupTextHoverType = {
-  container: HTMLElement | null;
-  type: FontWeightType;
+	container: HTMLElement | null;
+	type: FontWeightType;
+};
+
+export type WindowConfigItem = {
+	isOpen: boolean;
+	zIndex: number;
+	data: unknown | null;
+};
+
+type WindowKey = keyof typeof WINDOW_CONFIG;
+
+export type WindowStore = {
+	windows: typeof WINDOW_CONFIG;
+	nextZIndex: number;
+
+	openWindow: (windowKey: WindowKey, data?: unknown | null) => void;
+	closeWindow: (windowKey: WindowKey) => void;
+	focusWindow: (windowKey: WindowKey) => void;
 };
